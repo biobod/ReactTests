5f37cf17b9e5c77769fac87a73e5a5b6e8272a2b
Bogdan <info96532@gmail.com>
2016-08-19 17:22:11 +0300

    added tests (without check logic)

--- src/tests.js
+++ src/tests.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react'
 
+
 class TestQuestions extends Component{
   constructor(props) {
     super(props)
+    this.state = { questionNumber: 0 }
   }
-  render(){
-  return (
-    <div>
-      <form><p>Укажите ваш пол</p>
+  nextQuestion =()=> {
+    const { state } = this
+   this.setState({questionNumber: state.questionNumber +1})
 
+  }
+  questionBase = (questNumber) => {
+    const arr =  [<div className="question">
+      <form><p>Укажите ваш пол</p>
+        <label><input type="radio" name="gender" value="1"/>Malxe<br/></label>
+        <label><input type="radio" name="gender" value="2"/>Female<br/></label>
+        <label><input type="radio" name="gender" value="3"/>Other<br/></label>
 
       </form>
-    </div>
+    </div>,
+      <div className="question">
+        <form><p>Укажите ваш год</p>
+          <label><input type="radio" name="gender" value="1"/> 1999<br/></label>
+          <label><input type="radio" name="gender" value="2"/> 1223<br/></label>
+          <label><input type="radio" name="gender" value="3"/> 3c11<br/></label>
 
+        </form>
+      </div>,
+      <div className="question">
+        <form><p>Укажите ваш рост</p>
+          <label><input type="radio" name="gender" value="1"/> 155<br/></label>
+          <label><input type="radio" name="gender" value="2"/>180<br/></label>
+          <label><input type="radio" name="gender" value="3"/> 175<br/></label>
+
+        </form>
+      </div>]
+    return arr[questNumber]
+  }
+
+  render(){
+    const {
+        state: { questionNumber }
+    } = this
+  return (
+<div>
+   {this.questionBase(questionNumber)}
+  <input type="button"  value="Следующий вопрос" onClick={this.nextQuestion}/>
+</div>
   )
 }
 }
