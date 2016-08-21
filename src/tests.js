1e1c2bc60f17358077f1d1578968cddc8ebe3af6
Bogdan <info96532@gmail.com>
2016-08-21 21:53:56 +0300

    added radio-button module and result calculate logic

--- src/tests.js
+++ src/tests.js
@@ -1,40 +1,61 @@
 import React, { Component } from 'react'
+import {RadioGroup, Radio} from 'react-radio-group'
 
 
 class TestQuestions extends Component{
   constructor(props) {
     super(props)
-    this.state = { questionNumber: 0 }
+    this.state = {
+      questionNumber: 0,
+      value: 0,
+      resultValue: 0,
+
+    }
   }
   nextQuestion =()=> {
     const { state } = this
-   this.setState({questionNumber: state.questionNumber +1})
+    this.setState({questionNumber: state.questionNumber +1})
+    this.setState({resultValue: state.resultValue + +state.value})
+    this.setState({value: 0})
+    console.log(state.resultValue)
+
 
   }
+  saveValue = (value) => {
+    const { state } = this
+    this.setState({value: value});
+
+  }
+
   questionBase = (questNumber) => {
     const arr =  [<div className="question">
-      <form><p>Укажите ваш пол</p>
-        <label><input type="radio" name="gender" value="1"/>Malxe<br/></label>
-        <label><input type="radio" name="gender" value="2"/>Female<br/></label>
-        <label><input type="radio" name="gender" value="3"/>Other<br/></label>
+      <RadioGroup name="gender"  selectedValue={this.state.value } value={this.state.value} onChange={this.saveValue} >
+        <p>Укажите ваш пол</p>
+        <label><Radio value="1"/>Male<br/></label>
+        <label><Radio value="2"/>Female<br/></label>
+        <label><Radio value="3"/>Other<br/></label>
 
-      </form>
+
+      </RadioGroup>
     </div>,
       <div className="question">
-        <form><p>Укажите ваш год</p>
-          <label><input type="radio" name="gender" value="1"/> 1999<br/></label>
-          <label><input type="radio" name="gender" value="2"/> 1223<br/></label>
-          <label><input type="radio" name="gender" value="3"/> 3c11<br/></label>
+        <RadioGroup name="gender" selectedValue={this.state.value } value={this.state.value} onChange={this.saveValue}>
+          <p>Укажите ваш год</p>
+          <label><Radio value="1"/> 1999<br/></label>
+          <label><Radio value="2"/> 1223<br/></label>
+          <label><Radio value="3"/> 3121<br/></label>
 
-        </form>
+
+        </RadioGroup>
       </div>,
       <div className="question">
-        <form><p>Укажите ваш рост</p>
-          <label><input type="radio" name="gender" value="1"/> 155<br/></label>
-          <label><input type="radio" name="gender" value="2"/>180<br/></label>
-          <label><input type="radio" name="gender" value="3"/> 175<br/></label>
+        <RadioGroup name="gender" selectedValue={this.state.value} value={this.state.value} onChange={this.saveValue}>
+          <p>Укажите ваш рост</p>
+          <label><Radio value="1" />155<br/></label>
+          <label><Radio value="2" />180<br/></label>
+          <label><Radio value="3" />170<br/></label>
 
-        </form>
+        </RadioGroup>
       </div>]
     return arr[questNumber]
   }
@@ -44,10 +65,10 @@ class TestQuestions extends Component{
         state: { questionNumber }
     } = this
   return (
-<div>
-   {this.questionBase(questionNumber)}
-  <input type="button"  value="Следующий вопрос" onClick={this.nextQuestion}/>
-</div>
+  <div>
+    {this.questionBase(questionNumber)}
+  <input type="button"  value="Следующий sвопрос" onClick={this.nextQuestion} />
+  </div>
   )
 }
 }
