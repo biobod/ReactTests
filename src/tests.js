f6802b2437ef656a53e8aa84484054d29fe61655
Oleg Kalyta <2xyligan@gmail.com>
2016-08-22 16:38:34 +0300

    refactored tests

--- src/tests.js
+++ src/tests.js
@@ -1,76 +1,116 @@
-import React, { Component } from 'react'
-import {RadioGroup, Radio} from 'react-radio-group'
+import React, { Component, PropTypes } from 'react'
+import { RadioGroup, Radio } from 'react-radio-group'
 
+const Question = ({ selected, onChange, question, option1, option2, option3 }) => (
+  <div className="question">
+    <RadioGroup name="gender" selectedValue={selected} value={selected} onChange={onChange}>
+      <p>{question}</p>
+      <label>
+        <Radio value="1" />
+        {option1}
+      </label>
+      <br />
+      <label>
+        <Radio value="2" />
+        {option2}
+      </label>
+      <br />
 
-class TestQuestions extends Component{
+      <label>
+        <Radio value="3" />
+        {option3}
+      </label>
+      <br />
+    </RadioGroup>
+  </div>
+)
+
+Question.propTypes = {
+  selected: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]).isRequired,
+
+  onChange: PropTypes.func.isRequired,
+  question: PropTypes.string.isRequired,
+  option1: PropTypes.string.isRequired,
+  option2: PropTypes.string.isRequired,
+  option3: PropTypes.string.isRequired
+}
+
+class TestQuestions extends Component {
   constructor(props) {
     super(props)
     this.state = {
       questionNumber: 0,
       value: 0,
-      resultValue: 0,
-
+      resultValue: 0
     }
   }
-  nextQuestion =()=> {
-    const { state } = this
-    this.setState({questionNumber: state.questionNumber +1})
-    this.setState({resultValue: state.resultValue + +state.value})
-    this.setState({value: 0})
-    console.log(state.resultValue)
 
+  nextQuestion = () => {
+    const { questionNumber, resultValue, value } = this.state
+
+    this.setState({
+      questionNumber: questionNumber + 1,
+      resultValue: resultValue + +value,
+      value: 0
+    })
 
   }
-  saveValue = (value) => {
+  saveValue = value => {
     const { state } = this
-    this.setState({value: value});
-
+    this.setState({ value })
   }
 
   questionBase = (questNumber) => {
-    const arr =  [<div className="question">
-      <RadioGroup name="gender"  selectedValue={this.state.value } value={this.state.value} onChange={this.saveValue} >
-        <p>Укажите ваш пол</p>
-        <label><Radio value="1"/>Male<br/></label>
-        <label><Radio value="2"/>Female<br/></label>
-        <label><Radio value="3"/>Other<br/></label>
+    const {
+      state: { value },
+      saveValue
+    } = this
 
+    const arr = [
+      <Question
+        selected={value}
+        onChange={saveValue}
+        question="Укажите ваш пол"
+        option1="Male"
+        option2="Femail"
+        option3="Other"
+      />,
+      <Question
+        selected={value}
+        onChange={saveValue}
+        question="Укажите ваш год"
+        option1="1999"
+        option2="4821"
+        option3="5412"
+      />,
 
-      </RadioGroup>
-    </div>,
-      <div className="question">
-        <RadioGroup name="gender" selectedValue={this.state.value } value={this.state.value} onChange={this.saveValue}>
-          <p>Укажите ваш год</p>
-          <label><Radio value="1"/> 1999<br/></label>
-          <label><Radio value="2"/> 1223<br/></label>
-          <label><Radio value="3"/> 3121<br/></label>
+      <Question
+        selected={value}
+        onChange={saveValue}
+        question="Укажите ваш рост"
+        option1="174"
+        option2="169"
+        option3="184"
+      />]
 
-
-        </RadioGroup>
-      </div>,
-      <div className="question">
-        <RadioGroup name="gender" selectedValue={this.state.value} value={this.state.value} onChange={this.saveValue}>
-          <p>Укажите ваш рост</p>
-          <label><Radio value="1" />155<br/></label>
-          <label><Radio value="2" />180<br/></label>
-          <label><Radio value="3" />170<br/></label>
-
-        </RadioGroup>
-      </div>]
     return arr[questNumber]
   }
 
-  render(){
+  render() {
     const {
-        state: { questionNumber }
+      state: { questionNumber },
+      questionBase, nextQuestion,
     } = this
-  return (
-  <div>
-    {this.questionBase(questionNumber)}
-  <input type="button"  value="Следующий sвопрос" onClick={this.nextQuestion} />
-  </div>
-  )
-}
+    return (
+      <div>
+        {questionBase(questionNumber)}
+        <input type="button" value="Следующий sвопрос" onClick={nextQuestion} />
+      </div>
+    )
+  }
 }
 
 export default TestQuestions
